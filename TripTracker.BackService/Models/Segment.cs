18af547592e5c0ee201af80ec86b63e91bed4606
Carleton Snow <chip90@gmail.com>
2019-02-21 18:44:22 -0500

    Initial commit

---
+++ TripTracker.BackService/Models/Segment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TripTracker.BackService.Models
+{
+    public class Segment
+    {
+        public int Id { get; set; }
+
+        public int TripId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime StartDateTime { get; set; }
+
+        public DateTime EndDateTime { get; set; }
+
+    }
+}
